0422f79fc25b9cb470170446301b1a9c0de1e1f5
Petri Kivikangas <36138+Kitanotori@users.noreply.github.com>
2023-01-20 12:11:10 +0900

    Upgrade Gradle and migrate legacy syntax

--- src/main/java/dev/jacomet/gradle/plugins/logging/extension/LoggingCapabilitiesExtension.java
+++ src/main/java/dev/jacomet/gradle/plugins/logging/extension/LoggingCapabilitiesExtension.java
@@ -433,14 +433,14 @@ public class LoggingCapabilitiesExtension {
     private Action<Configuration> getSlf4JEnforcementSubstitutions() {
         return conf -> conf.getResolutionStrategy().dependencySubstitution(substitution -> {
             ComponentSelector log4JOverSlf4J = substitution.module(LoggingModuleIdentifiers.LOG4J_OVER_SLF4J.asFirstVersion());
-            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J.moduleId)).with(log4JOverSlf4J);
-            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J12API.moduleId)).with(log4JOverSlf4J);
+            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J.moduleId)).using(log4JOverSlf4J);
+            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J12API.moduleId)).using(log4JOverSlf4J);
 
-            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J_JUL.moduleId)).with(substitution.module(LoggingModuleIdentifiers.JUL_TO_SLF4J.asFirstVersion()));
+            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J_JUL.moduleId)).using(substitution.module(LoggingModuleIdentifiers.JUL_TO_SLF4J.asFirstVersion()));
 
             ComponentSelector jclOverSlf4J = substitution.module(LoggingModuleIdentifiers.JCL_OVER_SLF4J.asFirstVersion());
-            substitution.substitute(substitution.module(LoggingModuleIdentifiers.COMMONS_LOGGING.moduleId)).with(jclOverSlf4J);
-            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J_JCL.moduleId)).with(jclOverSlf4J);
+            substitution.substitute(substitution.module(LoggingModuleIdentifiers.COMMONS_LOGGING.moduleId)).using(jclOverSlf4J);
+            substitution.substitute(substitution.module(LoggingModuleIdentifiers.LOG4J_JCL.moduleId)).using(jclOverSlf4J);
         });
     }
 
