c0e57068bfde5fc8ce40de489b122861e9e45c77
Louis Jacomet <ljacomet@gmail.com>
2023-03-31 14:51:10 +0200

    Update license headers
    Change year to 2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -88,7 +88,7 @@ license {
         "groovy" to "SLASHSTAR_STYLE",
         "kts"    to "SLASHSTAR_STYLE"
     ))
-    ext.set("year", "2019")
+    ext.set("year", "2023")
     exclude("**/build/*")
     exclude("**/.gradle/*")
 }
